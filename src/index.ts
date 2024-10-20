63abb51b04088b74e501e4071c497bb73dff59fe
Olee <olee@Oles-Air.fritz.box>
2024-10-20 12:50:55 +0200

    Add basic working cutout script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,68 @@
-const sayHello = (name: string): string => {
-  return `Hello, ${name}!`;
+import sharp from "sharp";
+
+const [imagePath, horizontal, vertical] = process.argv.slice(2);
+
+if (!imagePath) {
+  console.error("Please provide a path to an image");
+  process.exit(1);
+}
+console.log({ imagePath });
+
+if (!horizontal || !vertical) {
+  console.warn(
+    "\nYou not provided any horizontal or vertical number of tiles. Assuming 1x1"
+  );
+}
+
+const horizontalTiles = horizontal ? parseInt(horizontal) : 1;
+const verticalTiles = vertical ? parseInt(vertical) : 1;
+console.log({ horizontalTiles, verticalTiles });
+
+const getRegions = (
+  width: number,
+  height: number,
+  horizontalTiles: number,
+  verticalTiles: number
+) => {
+  const tileWidth = Math.floor(width / horizontalTiles);
+  const tileHeight = Math.floor(height / verticalTiles);
+  const dimensions = { width: tileWidth, height: tileHeight };
+  const regions = [];
+  for (let i = 0; i < verticalTiles; i++) {
+    for (let j = 0; j < horizontalTiles; j++) {
+      const left = j * tileWidth;
+      const top = i * tileHeight;
+      regions.push({ left, top, ...dimensions });
+    }
+  }
+  return regions;
 };
 
-console.log(sayHello("World"));
+const main = async () => {
+  const metadata = await sharp(imagePath).metadata();
+  if (!metadata) {
+    console.error("Could not read metadata of the image");
+    process.exit(1);
+  }
+  console.log(`\nImage Dimensions: ${metadata.width}x${metadata.height}`);
+  const { width, height } = metadata;
+  if (!width || !height) {
+    console.error("Could not read width or height of the image");
+    process.exit(1);
+  }
+  const regions = getRegions(width, height, horizontalTiles, verticalTiles);
+  regions.forEach((region, index) => {
+    const { left, top, width, height } = region;
+    const outputPath = `./dist/${index}.png`;
+    sharp(imagePath)
+      .extract({ left, top, width, height })
+      .toFile(outputPath, (err, info) => {
+        if (err) {
+          console.error(`Error extracting region ${index}: ${err}`);
+          process.exit(1);
+        }
+        console.log(`Region ${index} extracted to ${outputPath}`);
+      });
+  });
+};
+main();
